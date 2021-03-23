ea815363ac905f2abc1edb25cc14964ede4dfbc3
VictorHugoMoreno <victormorenolhp@gmail.com>
2021-03-23 12:24:19 -0300

    Segundo commit

---
+++ src/main/java/br/ce/rest/OlaMundoTest.java
@@ -0,0 +1,25 @@
+package br.ce.rest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
+public class OlaMundoTest {
+	
+	@Test
+	public void testOlaMundo() {
+		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me:80/ola");
+		System.out.println(response.getBody().asString().equals("Ola Mundo!"));
+		System.out.println(response.statusCode() == 200);
+		
+		
+		ValidatableResponse validacao = response.then();
+//		System.out.println(" --- " + validacao.);
+		validacao.statusCode(200); 
+	}
+
+}
